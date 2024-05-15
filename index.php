445c548049c8bb055753b8d7bb75badbfc4ee4b9
jonas-amilton <jonas96@edu.unisinos.br>
2024-05-15 17:27:02 -0300

    style: adionando bootstrap

--- index.php
+++ index.php
@@ -5,30 +5,52 @@ require 'connection.php';
 $connection = new Connection();
 
 $users = $connection->query("SELECT * FROM users");
+?>
 
-echo "<table border='1'>
+<!DOCTYPE html>
+<html lang="pt">
 
-    <tr>
-        <th>ID</th>    
-        <th>Nome</th>    
-        <th>Email</th>
-        <th>Ação</th>    
-    </tr>
-";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
 
-foreach($users as $user) {
+<body>
+    <nav class="navbar bg-primary">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1 text-white">Home</span>
+            <a href="/inserir.php" style="color: #fff;">Inserir novo usuário</a>
+        </div>
+    </nav>
 
-    echo sprintf("<tr>
-                      <td>%s</td>
-                      <td>%s</td>
-                      <td>%s</td>
-                      <td>
-                           <a href='#'>Editar</a>
-                           <a href='#'>Excluir</a>
-                      </td>
-                   </tr>",
-        $user->id, $user->name, $user->email);
+    <div class="container-fluid d-flex justify-content-center">
+        <table class="table w-75">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user) : ?>
+                    <tr>
+                        <th scope="row"><?= $user->id; ?></th>
+                        <td><?= $user->name; ?></td>
+                        <td><?= $user->email; ?></td>
+                        <td>
+                            <a href='#'>Editar</a>
+                            <a href='#'>Excluir</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 
-}
+</body>
 
-echo "</table>";
+</html>
