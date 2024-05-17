feb640b6b21edae4814c7ecefa25b598221330e4
jonas-amilton <jonas96@edu.unisinos.br>
2024-05-17 16:47:00 -0300

    feat: Visualização de cores vinculadas

--- index.php
+++ index.php
@@ -5,6 +5,24 @@ require 'connection.php';
 $connection = new Connection();
 
 $users = $connection->query("SELECT * FROM users");
+
+function getColors($id)
+{
+    $connection = new Connection();
+
+    $query = "
+    SELECT colors.name 
+    FROM user_colors 
+    INNER JOIN colors ON user_colors.color_id = colors.id 
+    WHERE user_colors.user_id = $id
+";
+
+    $result = $connection->query($query);
+
+    foreach ($result as $row) {
+        echo ' - ' . $row->name;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -43,6 +61,7 @@ $users = $connection->query("SELECT * FROM users");
                     <th scope="col">ID</th>
                     <th scope="col">Nome</th>
                     <th scope="col">Email</th>
+                    <th scope="col">Cores Vinculadas</th>
                     <th scope="col">Ação</th>
                 </tr>
             </thead>
@@ -52,9 +71,22 @@ $users = $connection->query("SELECT * FROM users");
                     <td><?= $user->id; ?></td>
                     <td><?= $user->name; ?></td>
                     <td><?= $user->email; ?></td>
+                    <td><?= getColors($user->id); ?></td>
                     <td>
-                        <a href="/pages/editar.php?id=<?= $user->id; ?>" class="btn btn-primary btn-sm">Editar</a>
-                        <a href="/process_delete.php?id=<?= $user->id; ?>" class="btn btn-danger btn-sm">Excluir</a>
+                        <a href="/pages/editar.php?id=<?= $user->id; ?>" class="btn btn-primary btn-sm"><svg
+                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                <path fill-rule="evenodd"
+                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                            </svg></a>
+                        <a href="/process_delete.php?id=<?= $user->id; ?>" class="btn btn-danger btn-sm"><svg
+                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                            </svg></a>
                     </td>
                 </tr>
                 <?php endforeach; ?>
