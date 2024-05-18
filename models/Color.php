87441974da4e0ba92693d197ba755816476063ee
jonas-amilton <jonas96@edu.unisinos.br>
2024-05-18 15:14:45 -0300

    feat: Criado método para retornar todas as cores da tabela colors

--- models/Color.php
+++ models/Color.php
@@ -35,4 +35,20 @@ class Color
             return false;
         }
     }
+
+    public function getColors()
+    {
+        try {
+            $connection = new Connection();
+
+            $query = "SELECT * FROM colors";
+
+            $result = $connection->getConnection()->query($query);
+            $result->setFetchMode(PDO::FETCH_INTO, new stdClass);
+            return $result;
+        } catch (PDOException $e) {
+            echo "Erro ao executar a consulta: " . $e->getMessage();
+            return false;
+        }
+    }
 }
