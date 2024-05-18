ba0e4869ec11e090a6b89cf6c123c18787c4b71e
jonas-amilton <jonas96@edu.unisinos.br>
2024-05-18 19:47:04 -0300

    feat: Criado rotas para lidar com requests

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require './routes.php';
+
 include_once('./models/Color.php');
 include_once('./models/User.php');
 
@@ -48,12 +50,13 @@ include_once("./layouts/_header.php");
                             </svg>
                         </button>
                     </form>
-                    <a href="/process_delete.php?id=<?= $user->id; ?>" class="btn btn-danger btn-sm"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    <a href="index.php?action=delete&id=<?= $user->id; ?>" class="btn btn-danger btn-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path
                                 d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-                        </svg></a>
+                        </svg>
+                    </a>
                 </td>
             </tr>
             <?php endforeach; ?>
---
+++ routes.php
@@ -0,0 +1,24 @@
+<?php
+require './controllers/UserController.php';
+require './controllers/ColorController.php';
+
+$userController = new UserController();
+$colorController = new ColorController();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['id']) && isset($_POST['name']) && isset($_POST['email'])) {
+        $userController->updateUser();
+    } elseif (isset($_POST['name']) && isset($_POST['email'])) {
+        $userController->insertUser();
+    } elseif (isset($_POST['color_id']) && isset($_POST['user_id'])) {
+        $colorController->linkColor();
+    } else {
+        echo "Dados obrigatórios não foram fornecidos.";
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['action']) && $_GET['action'] === 'delete' && isset($_GET['id'])) {
+        $userController->deleteUser();
+    }
+} else {
+    echo "Método de requisição não suportado.";
+}
