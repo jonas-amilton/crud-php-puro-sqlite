1d5cbf166680f3302328258db0eb026af3979da8
jonas-amilton <jonas96@edu.unisinos.br>
2024-05-19 15:50:23 -0300

    feat: Criado botão para desvincular cor de usuário

--- frontend/views/layouts/_container_cor.php
+++ frontend/views/layouts/_container_cor.php
@@ -12,17 +12,26 @@ $colors = (array) $modelColor->getColorsByIdUser($userId);
         <div class="card-body">
             <h5 class="card-title text-center"><?= 'Cores vinculadas a ' . $_POST['name'] ?? '' ?></h5>
             <?php if (!empty($colors)) : ?>
-            <?php foreach ($colors as $color) : ?>
-            <hr>
-            <div class="d-flex align-items-center justify-content-between">
-                <div><?= $color['name'] ?>
-                </div>
-                <div style="background-color: <?= $color['name']; ?>; width: 1em; height: 1em; border-radius: 50%;">
-                </div>
-            </div>
-            <?php endforeach ?>
+                <?php foreach ($colors as $color) : ?>
+                    <hr>
+                    <form action="/index.php" method="post">
+                        <div class="d-flex justify-content-between">
+                            <div class="d-flex align-items-center">
+                                <input hidden name="userId" type="text" value="<?= $userId; ?>">
+                                <input hidden name="colorId" type="text" value="<?= ($color['color_id']) ?>">
+                                <div class="m-1"><?= $color['name'] ?>
+                                </div>
+                                <div style="background-color: <?= $color['name']; ?>; width: 1em; height: 1em; border-radius: 50%;">
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-danger btn-sm">
+                                Desvincular cor
+                            </button>
+                        </div>
+                    </form>
+                <?php endforeach ?>
             <?php else : ?>
-            Nenhuma cor vinculada
+                Nenhuma cor vinculada
             <?php endif ?>
         </div>
     </div>
