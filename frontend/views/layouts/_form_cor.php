dc984c633a5f3ce720854d44a2cc1b83d69d763d
jonas-amilton <jonas96@edu.unisinos.br>
2024-05-19 15:06:00 -0300

    chore: Adicionado concatenação para exibir nome do usuário

--- frontend/views/layouts/_form_cor.php
+++ frontend/views/layouts/_form_cor.php
@@ -7,14 +7,14 @@ $modelColor = new Color();
 <div class="col-md-6">
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title text-center">Vincular cor</h5>
+            <h5 class="card-title text-center"><?= 'Vincular cor a ' . $_POST['name'] ?? '' ?></h5>
             <form action="../../../index.php" method="post">
                 <div class="mb-3">
                     <input value="<?= $_POST['id']; ?>" hidden type="text" name="user_id">
                     <select name="color_id" class="form-select" aria-label="default select example">
                         <option value="n/a" selected>Escolha uma opção</option>
                         <?php foreach ($modelColor->getColors() as $color) : ?>
-                        <option value="<?= $color->id; ?>"><?= $color->name; ?></option>
+                            <option value="<?= $color->id; ?>"><?= $color->name; ?></option>
                         <?php endforeach; ?>
                     </select>
                 </div>
