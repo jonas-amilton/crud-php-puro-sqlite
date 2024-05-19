c961001d701368c269e304471fbe979f55574f80
jonas-amilton <jonas96@edu.unisinos.br>
2024-05-19 15:02:38 -0300

    refactor: Nesta refatoração os icones foram substituídos por textos, foi removido as cores vinculadas da home e adicionado um link para página vincular cores

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 require './routes.php';
 
-include_once('./backend/models/Color.php');
 include_once('./backend/models/User.php');
 
-$modelColor = new Color();
 $modelUser = new User();
 ?>
 
@@ -16,14 +14,13 @@ $textoHref = "Inserir novo usuário";
 include_once("./frontend/views/layouts/_header.php");
 ?>
 
-<div class="container mt-4">
+<div class="container mt-4 text-center text-center">
     <table class="table table-striped">
         <thead>
             <tr>
                 <th scope="col">ID</th>
                 <th scope="col">Nome</th>
                 <th scope="col">Email</th>
-                <th scope="col">Cores Vinculadas</th>
                 <th scope="col">Ação</th>
             </tr>
         </thead>
@@ -33,28 +30,24 @@ include_once("./frontend/views/layouts/_header.php");
                 <td><?= $user->id; ?></td>
                 <td><?= $user->name; ?></td>
                 <td><?= $user->email; ?></td>
-                <td><?= $modelColor->getColorsByIdUser($user->id); ?></td>
-                <td>
+                <td class="d-flex justify-content-around">
+                    <form action="/frontend/views/pages/vincular-cores.php" method="post">
+                        <input type="hidden" name="id" value="<?= $user->id ?>">
+                        <input type="hidden" name="name" value="<?= $user->name ?>">
+                        <button type="submit" class="btn btn-secondary btn-sm">
+                            Vincular cores
+                        </button>
+                    </form>
                     <form action="/frontend/views/pages/editar.php" method="post">
                         <input type="hidden" name="name" value="<?= $user->name ?>">
                         <input type="hidden" name="email" value="<?= $user->email ?>">
                         <input type="hidden" name="id" value="<?= $user->id ?>">
                         <button type="submit" class="btn btn-primary btn-sm">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                <path
-                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                <path fill-rule="evenodd"
-                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
-                            </svg>
+                            Editar usuário
                         </button>
                     </form>
                     <a href='index.php?action=delete&id=<?= $user->id; ?>' class="btn btn-danger btn-sm">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-trash-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-                        </svg>
+                        Deletar Usuário
                     </a>
                 </td>
             </tr>
